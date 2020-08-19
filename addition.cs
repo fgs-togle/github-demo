1bdbb056df832e15dbddfb18a1c6fef8bc11c3ee
Tom Ogle <togle@foxguardsolutions.com>
2020-08-19 16:01:54 -0400

    Added additon file

---
+++ addition.cs
@@ -0,0 +1,10 @@
+using System;
+public class test
+{   
+    public void Main()
+    {
+        Console.WriteLine("Test");
+        Console.WriteLine("Here is an addtion");
+        Console.WriteLine("Here is more work to do");
+    }
+}
