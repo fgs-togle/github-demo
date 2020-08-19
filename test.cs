206235f904f4ec6580f91f886db5047c89ea8f8f
Tom Ogle <togle@foxguardsolutions.com>
2020-08-19 16:02:27 -0400

    Added test file

---
+++ test.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Test
+{
+    public void Main()
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Rock!");
+        Console.WriteLine("Roll!");
+    }
+}
